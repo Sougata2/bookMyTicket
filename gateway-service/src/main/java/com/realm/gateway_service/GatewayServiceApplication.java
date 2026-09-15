1d328b980f3b28735f3bcb0582d44221a1c4440d
sougata <sougata19582001@gmail.com>
2026-09-15 23:52:11 +0530

    added jwt filter

--- gateway-service/src/main/java/com/realm/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/realm/gateway_service/GatewayServiceApplication.java
@@ -3,11 +3,11 @@ package com.realm.gateway_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.realm.jwt_security"})
 public class GatewayServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
 
 }
