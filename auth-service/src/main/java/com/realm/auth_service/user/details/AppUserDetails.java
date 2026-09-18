2d4c9796ce7cb2e4d85e8f881feb99db3de4b169
sougata <sougatadas@cdac.in>
2026-09-18 17:01:50 +0530

    added app user details class

---
+++ auth-service/src/main/java/com/realm/auth_service/user/details/AppUserDetails.java
@@ -0,0 +1,30 @@
+package com.realm.auth_service.user.details;
+
+import com.realm.auth_service.user.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class AppUserDetails implements UserDetails {
+    private final UserEntity user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
