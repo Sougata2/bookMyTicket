dae98b3bdb504522d2a3c8a94068a2497ad59c5b
sougata <sougatadas@cdac.in>
2026-09-24 16:59:29 +0530

    added jwt-security dependency

--- auth-service/src/main/java/com/realm/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/realm/auth_service/AuthServiceApplication.java
@@ -3,11 +3,11 @@ package com.realm.auth_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.realm.jwt_security"})
 public class AuthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
 
 }
